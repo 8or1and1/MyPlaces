3fd9074dacd5b33293a506cc15099ac983d45679
8or1and1 <8or1and1@gmail.com>
2022-05-12 13:42:46 +0300

    initial

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+	
+	let restaurantNames = [
+		"Napoli", "KFC", "Волга-Волга", "Додо пицца", "Папа Панда",
+		"Сушивёсла", "Ахтуба", "AIK", "Шаурма TJ", "MixFood",
+		"AEG Pizza", "Beer Meister", "Lebrok", "Два ежа", "Draft punk"
+	]
 
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
-
-
+	
+	// MARK: - Table view data source
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return restaurantNames.count
+	}
+	
+	override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 60
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		var content = cell.defaultContentConfiguration()
+		content.text = restaurantNames[indexPath.row]
+		content.image = UIImage(named: restaurantNames[indexPath.row])
+		cell.contentConfiguration = content
+		return cell
+	}
 }
 
